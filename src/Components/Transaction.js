1a5131c03ca823aa12a5f3c006ac248524d1d46b
Amin Koupaie <48063971+amin-aoka@users.noreply.github.com>
2023-03-05 08:44:40 +0000

    icon added

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,8 +1,11 @@
+import {FaTimes} from 'react-icons/fa'
+
 function Transaction ({transaction}){
     return(
         <div className="transaction">
             <span>{transaction.text}</span>
             <span>{transaction.amount}</span>
+            <FaTimes/>
         </div>
     )
 }
