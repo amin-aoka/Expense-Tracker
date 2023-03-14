6a41b5143ce96ad4f2afe4dcc9e3254542013a61
AOKA <amin.aoka@gmail.com>
2023-03-14 12:23:53 +0330

    LABELS ADDED

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,11 +1,13 @@
 import {FaTimes} from 'react-icons/fa';
 
 function Transaction ({transaction, onClick}){
+    
     transaction.amount=+transaction.amount
     return(
-        <div className="transaction">
+        <div className= {"transaction"}>
             <span>{transaction.text}</span>
             <span> ${transaction.amount.toFixed(2)}</span>
+            <span className={transaction.amount >0? "incomeMarker": "expenseMarker"}> </span>
             <FaTimes onClick= {()=> onClick(transaction.id)} className = "closeIcon"/>
         </div>
     )
